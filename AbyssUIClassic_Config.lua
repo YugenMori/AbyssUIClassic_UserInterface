019265aa835dbf4423f41b37a8e52442bb05f449
YugenMori <22729525+YugenMori@users.noreply.github.com>
2019-07-07 21:49:29 -0300

    Retail imports.

--- AbyssUIClassic_Config.lua
+++ AbyssUIClassic_Config.lua
@@ -946,6 +946,16 @@ AbyssUIClassic_HideInCombat_CheckButton:SetChecked(AbyssUIClassicAddonSettings.E
 AbyssUIClassic_HideInCombat_CheckButton:SetScript("OnClick", function(self)
   AbyssUIClassicAddonSettings.ExtraFunctionHideInCombat = self:GetChecked()
 end)
+-- American Clock Style --
+local AbyssUI_AmericanClock_CheckButton = CreateFrame("CheckButton", "$parentAbyssUI_AmericanClock_CheckButton", AbyssUI_Config.childpanel3, "ChatConfigCheckButtonTemplate")
+AbyssUI_AmericanClock_CheckButton:SetPoint("TOPLEFT", 180, -170)
+AbyssUI_AmericanClock_CheckButton.Text:SetText("American Date Style")
+AbyssUI_AmericanClock_CheckButton.tooltip = "Change the date format of all UI to the American style"
+AbyssUI_AmericanClock_CheckButton:SetChecked(AbyssUIAddonSettings.ExtraFunctionAmericanClock)
+-- OnClick Function
+AbyssUI_AmericanClock_CheckButton:SetScript("OnClick", function(self)
+  AbyssUIAddonSettings.ExtraFunctionAmericanClock = self:GetChecked()
+end)
 --End
 ----------------------------- AbyssUIClassic Stylization ------------------------------
 -- Player Portrait Style --
